3c55960fb91ec088e3e062c55c4edd45bf57972e
HikkusT <luizhenrique98@gmail.com>
2019-02-06 01:22:12 -0200

    Improved wave generation

--- Assets/01WaterSimulation/Water.cs
+++ Assets/01WaterSimulation/Water.cs
@@ -6,6 +6,7 @@ public class Water : MonoBehaviour {
 
 	public float MedWavelenght;
 	public float MedAmplitude;
+	public float Decay;
 
 	public Material WaterMat;
 
@@ -19,9 +20,24 @@ public class Water : MonoBehaviour {
 	}
 	
 	void Update () {
+		UpdateWaves();
+
 		WaterMat.SetInt("_NumWaves", numWaves);
 		WaterMat.SetVectorArray("_WaveData", waveData);
 		WaterMat.SetVectorArray("_WaveDirections", waveDirections);
+	}
+
+	void UpdateWaves()
+	{
+		for(int i = 0; i < numWaves; i ++)
+		{
+			Vector4 wave = waveData[i];
+			wave.w -= Decay * Time.deltaTime;
+			if (wave.w < 0.01f)
+				KillWave(i);
+			else
+				waveData[i] = wave;
+		}
 	}
 
 	void SpawnWave()
@@ -33,8 +49,15 @@ public class Water : MonoBehaviour {
 		float speed = Mathf.Sqrt(9.8f * wavelenght / (2f * Mathf.PI));
 		float phi = speed * frequency;
 		
-		waveData.Add(new Vector4(amplitude, frequency, phi, 1));
-		waveDirections.Add(new Vector4(1, 1, 0, 0));
+		waveData.Add(new Vector4(amplitude, frequency, speed, 1));
+		waveDirections.Add(new Vector4(1, 0, 0, 1));
 		numWaves++;
+	}
+
+	void KillWave(int i)
+	{
+		waveData.RemoveAt(i);
+		waveDirections.RemoveAt(i);
+		numWaves --;
 	}
 }
