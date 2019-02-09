501ba6ed40d5913d71efd61e85a95c38813380f9
HikkusT <luizhenrique98@gmail.com>
2019-02-09 00:39:34 -0200

    Improve wind system

--- Assets/01WaterSimulation/Water.cs
+++ Assets/01WaterSimulation/Water.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Water : MonoBehaviour {
 
+	enum WaveState {rising, dying}
+
 	public float MedWavelenght;
 	public float MedAmplitude;
 	public float Decay;
@@ -15,16 +17,19 @@ public class Water : MonoBehaviour {
 	List<Vector4> waveData = new List<Vector4>();
 	List<Vector4> waveDirections = new List<Vector4>();
 	List<float> waveIntensity = new List<float>();
+	List<WaveState> waveState = new List<WaveState>();
 	Vector2 windDir;
 
 	void Start () {
+		FillLists(20);
 		windDir = Random.insideUnitCircle;
-		for (int i = 0; i < 4; i ++)
-			SpawnWave();
+		StartCoroutine(SpawnInitialWaves());
 	}
 	
 	void Update () {
+		UpdateWind();
 		UpdateWaves();
+		Debug.Log(waveIntensity.Count.ToString());
 
 		WaterMat.SetInt("_NumWaves", numWaves);
 		WaterMat.SetVectorArray("_WaveData", waveData);
@@ -32,22 +37,39 @@ public class Water : MonoBehaviour {
 		WaterMat.SetFloatArray("_WaveIntensity", waveIntensity);
 	}
 
+	void UpdateWind()
+	{
+		float angle  = Mathf.Deg2Rad * Random.Range(0, 10) * Time.deltaTime;
+		windDir = RotateVector2D(windDir, angle); 
+	}
+
 	void UpdateWaves()
 	{
 		for(int i = 0; i < numWaves; i ++)
 		{
-			waveIntensity[i] -= Decay * Time.deltaTime;
-			if (waveIntensity[i] < 0.01f)
-				KillWave(i);
+			if(waveState[i] == WaveState.dying)
+			{
+				waveIntensity[i] -= Decay * Time.deltaTime;
+				if (waveIntensity[i] < 0.01f)
+				{
+					KillWave(i);
+					SpawnWave();
+				}
+			}
+			else
+			{
+				waveIntensity[i] += Decay * Time.deltaTime;
+				if (waveIntensity[i] > 0.99f)
+					waveState[i] = WaveState.dying;
+			}
 		}
 	}
 
 	Vector2 GetRandomWaveDir()
 	{
-		float angle  = Mathf.Deg2Rad * Random.Range(- WindCone, WindCone);
-		float x = windDir.x * Mathf.Cos(angle) - windDir.y * Mathf.Sin(angle);
-		float y = windDir.x * Mathf.Sin(angle) + windDir.y * Mathf.Cos(angle);
-		return new Vector2(x, y);
+		float angle = Mathf.Deg2Rad * Random.Range(- WindCone, WindCone);
+
+		return RotateVector2D(windDir, angle);
 	}
 
 	void SpawnWave()
@@ -59,18 +81,56 @@ public class Water : MonoBehaviour {
 		float speed = Mathf.Sqrt(9.8f * wavelenght / (2f * Mathf.PI));
 		float phi = speed * frequency;
 		
-		waveData.Add(new Vector4(amplitude, frequency, speed, 2));
-		waveIntensity.Add(1f);
+		waveData[numWaves] = new Vector4(amplitude, frequency, speed, 2);
+		waveIntensity[numWaves] = 0f;
 		Vector2 waveDir = GetRandomWaveDir();
-		waveDirections.Add(new Vector4(waveDir.x, waveDir.y, 0, 1));
+		waveDirections[numWaves] = new Vector4(waveDir.x, waveDir.y, 0, 1);
+		waveState.Add(WaveState.rising);
 		numWaves++;
 	}
 
 	void KillWave(int i)
 	{
+		RemoveAtIndex(i);
+		numWaves --;
+	}
+
+	IEnumerator SpawnInitialWaves()
+	{
+		for (int i = 0; i < 4; i ++)
+		{
+			SpawnWave();
+			yield return new WaitForSeconds(Random.Range(2f, 5f));
+		}
+	}
+
+	Vector2 RotateVector2D (Vector2 vector, float angle)
+	{
+		float x = vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle);
+		float y = vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle);
+
+		return new Vector2(x, y);
+	}
+
+	void FillLists(int size)
+	{
+		for (int i = 0; i < size; i ++)
+		{
+			waveData.Add(new Vector4(0, 0, 0, 0));
+			waveIntensity.Add(0);
+			waveDirections.Add(new Vector4(0, 0, 0, 0));
+		}
+	}
+
+	void RemoveAtIndex(int i)
+	{
 		waveData.RemoveAt(i);
 		waveDirections.RemoveAt(i);
 		waveIntensity.RemoveAt(i);
-		numWaves --;
+		waveState.RemoveAt(i);
+
+		waveData.Add(new Vector4(0, 0, 0, 0));
+		waveIntensity.Add(0);
+		waveDirections.Add(new Vector4(0, 0, 0, 0));
 	}
 }
